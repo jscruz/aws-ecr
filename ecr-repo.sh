922d8a90e691974e0c27451ad50e9b6aac4dfb5d
Jorge Cruz-Lambert <jscruzlambert@gmail.com>
2019-03-29 16:08:14 +0000

    Added region parameter
    Added region parameter. Default to use AWS EC2 metadata.

--- ecr-repo.sh
+++ ecr-repo.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-aws ecr describe-repositories --repository-names $1 2>&1 > /dev/null
+if [ -z "$2" ]
+  then
+    echo "No region specified. Extracting from AWS EC2 metadata"
+    REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+else
+    REGION="$2"
+fi
+
+aws ecr describe-repositories --region $REGION --repository-names $1 2>&1 > /dev/null
 status=$?
 if [[ ! "${status}" -eq 0 ]]; then
-    aws ecr create-repository --repository-name $1
+    aws ecr create-repository --region $REGION --repository-name $1
 fi
