da3179c080bedc6cd69c0f7449b3921be7cb4bba
Jorge Cruz Lambert <jorge.cruz_lambert@yunextraffic.com>
2023-10-17 13:45:13 +0100

    Add set policy option

--- ecr-repo.sh
+++ ecr-repo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PERMISSIONS_FILE=/tmp/permissions.json
 
 if [ -z "$2" ]
   then
@@ -8,11 +9,18 @@ else
     REGION="$2"
 fi
 
+if [ -f "$PERMISSIONS_FILE" ]; then
+    echo "Permissions file $FILE exists."
+fi
+
 echo "Region selected $REGION"
 aws ecr describe-repositories --region $REGION --repository-names $1 2>&1 > /dev/null
 status=$?
 if [[ ! "${status}" -eq 0 ]]; then
     aws ecr create-repository --region $REGION --repository-name $1
+    if [ -f "$PERMISSIONS_FILE" ]; then
+       aws ecr set-repository-policy --region $REGION --repository-name $1 --policy-text "file://$PERMISSIONS_FILE"
+    fi
 else
     echo "ECR Repository $1 already exists"
 fi
